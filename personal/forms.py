6f87b2c3f4e994194b5f0258f70ea6306a104447
manvith <iib2019014@iiita.ac.in>
2021-05-22 07:52:20 +0530

    1) all buildings and their details are shown in admin page 2) update links for buildings are given

--- personal/forms.py
+++ personal/forms.py
@@ -1,4 +1,5 @@
 from django.forms import ModelForm, TextInput, forms
+from django.forms.widgets import NumberInput
 from .models import NameRequest, CoordRequest, BuildingRequest, Building
 
 class NameRequestForm(ModelForm):
@@ -26,7 +27,14 @@ class BuildingForm(ModelForm) :
         model = Building
         fields = ['building_name', 'building_id', 'owner_name', 'longitude', 'latitude', 'yesterday_min', 'yesterday_max', 'db_yesterday_min', 'db_yesterday_max']
         widgets = { 'building_name' : TextInput(attrs={'class' : 'input', 'placeholder' : 'Building Name'}),
-                    'building_id' : TextInput(attrs={'class' : 'input', 'placeholder' : 'Place latitude'})
+                    'building_id' : TextInput(attrs={'class' : 'input', 'placeholder' : 'Building ID'}),
+                    'owner_name' : TextInput(attrs={'class' : 'input', 'placeholder' : 'Owner Name'}),
+                    'longitude' : TextInput(attrs={'class' : 'input', 'placeholder' : 'longitude'}),
+                    'latitude' : TextInput(attrs={'class' : 'input', 'placeholder' : 'latitude'}),
+                    'yesterday_min' : NumberInput(attrs={'class' : 'input', 'placeholder' : 'yesterday_min'}),
+                    'yesterday_max' : NumberInput(attrs={'class' : 'input', 'placeholder' : 'yesterday_max'}),
+                    'db_yesterday_min' : NumberInput(attrs={'class' : 'input', 'placeholder' : 'db_yesterday_min'}),
+                    'db_yesterday_max' : NumberInput(attrs={'class' : 'input', 'placeholder' : 'db_yesterday_max'}),
                     }
 
 class BuildingUpdateForm(ModelForm) :
