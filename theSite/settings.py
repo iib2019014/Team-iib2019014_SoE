a55526cd74c3175130c3260d1d3865a34d6479ed
manvith <iib2019014@iiita.ac.in>
2021-05-22 08:00:13 +0530

    1) removed media folders from src 2) removed media urls and roots from settings.py file

--- theSite/settings.py
+++ theSite/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 
 STATICFILES_DIRS =[
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media'),
+    # os.path.join(BASE_DIR, 'media'),
 ]
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+# MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
-MODEL_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+# MODEL_ROOT = os.path.join(BASE_DIR, 'media_cdn')
--- theSite/urls.py
+++ theSite/urls.py
@@ -122,4 +122,4 @@ urlpatterns = [
 
 if settings.DEBUG : # if we are in developement env,
     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    # urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
